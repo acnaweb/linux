2f29a3bc3c858900f9b1f741a8f579830b0dfdfc
AC <ac@marketmining.com.br>
2019-03-04 20:24:51 -0300

    update

--- debian-install.sh
+++ debian-install.sh
@@ -9,8 +9,8 @@ apt -y install make perl
 # apt install --fix-missing
 apt -y install build-essential
 
-apt-get install firmware-linux-nonfree
-apt-get install firmware-misc-nonfree
+apt install firmware-linux-nonfree
+apt install firmware-misc-nonfree
 
 # Lenovo
 # apt -y install firmware-atheros firmware-realtek
@@ -22,32 +22,34 @@ apt-get install firmware-misc-nonfree
 # aptitude install build-essential
 
 
-sudo dpkg --add-architecture i386
+dpkg --add-architecture i386
 apt install llvm-4.0 clang-4.0
 
 reboot
 
 # ifconfig, arq, netstat, rarp, nameif, route, traceroute
-sudo apt -y install net-tools
+apt -y install net-tools
+apt -y install telnet
+
 
 # show open ports : nmap [ip]
-sudo apt -y install nmap
+apt -y install nmap
 
 # fix gpg: failed to start the dirmngr '/usr/bin/dirmngr': Arquivo ou diretório não encontrado
-sudo apt -y install dirmngr 
+apt -y install dirmngr 
 
 # fix: Failed to load module "canberra-gtk-module"
-sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
+apt install libcanberra-gtk-module libcanberra-gtk3-module
 
 # Configuring the Firewall
-# sudo apt -y install ufw
+# apt -y install ufw
 
 
-echo 'deb http://ftp.debian.org/debian stretch-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
-sudo apt update
-sudo apt -y install -t stretch-backports remmina remmina-plugin-rdp remmina-plugin-secret
+echo 'deb http://ftp.debian.org/debian stretch-backports main' | tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
+apt update
+apt -y install -t stretch-backports remmina remmina-plugin-rdp remmina-plugin-secret
 
-sudo apt -y install git
-sudo apt -y install python-iniparse
+apt -y install git
+apt -y install python-iniparse
 
 
